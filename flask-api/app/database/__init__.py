0a18cb10a21ab5964142c827b62e7ee2d36a9270
Gabriel <gabriel@quickup.io>
2023-02-09 17:46:29 -0300

    adiciona função para pegar transações no banco de dados a partir do id do user

--- flask-api/app/database/__init__.py
+++ flask-api/app/database/__init__.py
@@ -23,3 +23,21 @@ class DataBaseConnection:
         result = conn.execute(expression)
 
         return result.fetchall()
+
+    def select_transactions(account_id):
+        from app.models.transaction import Transaction
+        from sqlalchemy.sql import select
+        from sqlalchemy import create_engine, or_
+    
+        engine = create_engine('mysql+pymysql://gabriel:143867@0.0.0.0:3306/mysql')
+        expression = select(Transaction).filter(or_(
+                        Transaction.transfer_sender_id == account_id, 
+                        Transaction.transfer_receiver_id == account_id,         
+                        )).limit(10)
+
+        conn = engine.connect()
+        result = conn.execute(expression)
+
+        return result.fetchall()
+
+
