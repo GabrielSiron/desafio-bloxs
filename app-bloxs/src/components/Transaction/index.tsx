69837267e73140a69e12bd313b323fd0c8cc3181
Gabriel <gabriel@quickup.io>
2023-02-09 17:42:43 -0300

    Adiciona texto dinâmico ao Componente de Transação

--- app-bloxs/src/components/Transaction/index.tsx
+++ app-bloxs/src/components/Transaction/index.tsx
@@ -1,9 +1,11 @@
-import { Element } from './style';
+import { Element, Title, Value } from './style';
 
 const Transaction = (props: any) => {
+    
     return(
         <Element>
-            
+            <Title>{props.transaction.is_sender? 'Transferência Enviada' : 'Transferência Recebida'}</Title>
+            <Value>{props.transaction.value}</Value>
         </Element>
     )
 }
--- app-bloxs/src/components/Transaction/style.ts
+++ app-bloxs/src/components/Transaction/style.ts
@@ -7,3 +7,18 @@ export const Element = styled.div `
     background-color: #fff;
     border-radius: 12px;
 `
+
+export const Title = styled.h2 `
+    font-family: 'poppins';
+    font-size: 1.2rem;
+    width: 60%;
+    padding: 12px;
+    float: left;
+`
+
+export const Value = styled.span `
+    font-family: 'poppins';
+    font-size: 1.0rem;
+    padding: 20px;
+    float: right;
+`
