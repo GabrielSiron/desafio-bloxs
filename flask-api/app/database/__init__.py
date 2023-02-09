527bb871bc5f5f95d6869e23137c49190f5944dc
Gabriel <gabriel@quickup.io>
2023-02-09 14:04:19 -0300

    add função select_many para comunicação com db

--- flask-api/app/database/__init__.py
+++ flask-api/app/database/__init__.py
@@ -11,3 +11,15 @@ class DataBaseConnection:
         result = conn.execute(expression)
 
         return result.fetchone()
+    
+    @staticmethod
+    def select_many(_class, comparator_object):
+        from sqlalchemy.sql import select
+        from sqlalchemy import create_engine
+    
+        engine = create_engine('mysql+pymysql://gabriel:143867@0.0.0.0:3306/mysql')
+        expression = select(_class).filter_by(**comparator_object)
+        conn = engine.connect()
+        result = conn.execute(expression)
+
+        return result.fetchall()
