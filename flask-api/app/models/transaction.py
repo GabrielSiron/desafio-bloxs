2a72c55b37e9f74f5c83086f8549af02bcceff2d
Gabriel <gabriel@quickup.io>
2023-02-09 14:06:55 -0300

    ajusta filtro de transferências realizadas

--- flask-api/app/models/transaction.py
+++ flask-api/app/models/transaction.py
@@ -5,16 +5,19 @@ from app.models.base_class import BaseClass
 class Transaction(db.Model, BaseClass):
     value = db.Column(db.Float, nullable=False)
     transaction_date = db.Column(db.DateTime, nullable=False)
-    account_id = db.Column(db.ForeignKey('account.id'), nullable=False)
-    account = db.relationship('Account', foreign_keys=account_id, order_by='Account.id')
+    transfer_sender_id = db.Column(db.ForeignKey('account.id'), nullable=False)
+    transfer_sender = db.relationship('Account', foreign_keys=transfer_sender_id, order_by='Account.id')
+    transfer_receiver_id = db.Column(db.ForeignKey('account.id'), nullable=False)
+    transfer_receiver = db.relationship('Account', foreign_keys=transfer_receiver_id, order_by='Account.id')
     
     @classmethod
-    def get_transactions(cls, page):
+    def get_transactions(cls, page, account_id):
+        from app.database import DataBaseConnection
 
-        transactions = cls.query.paginate(page=page, per_page=1)
-        print(transactions)
+        sender_transfereces = DataBaseConnection.select_many(Transaction, { 'transfer_sender_id': account_id })
 
-        return transactions
+        # receiver_transfereces = DataBaseConnection.select_many(Transaction, { 'transfer_receiver_id': account_id })
+        return sender_transfereces
     
     def to_json(self):
         return {
