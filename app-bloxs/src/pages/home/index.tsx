6d547f3d8a360dab77491427a96ed3d59bdb58fb
Gabriel <gabriel@quickup.io>
2023-02-09 17:43:16 -0300

    Adiciona verificaçãod de user logado

--- app-bloxs/src/pages/home/index.tsx
+++ app-bloxs/src/pages/home/index.tsx
@@ -8,16 +8,31 @@ import { UserContext } from '../../contexts/user';
 import { useState, useContext, useEffect } from 'react';
 import { Get, GetFirstPage } from '../../services/crud';
 import Transaction from '../../components/Transaction/index';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const { email, password, name, cpf, setToken, setEmail, setPassword } = useContext(UserContext);
+    const { email, password, name, cpf, token, setToken, setEmail, setPassword } = useContext(UserContext);
 
     const [loading, setLoading] = useState(false);
     const [account, setAccount] = useState({message: '', amount: 0, name: ''});
     const [transactions, setTransactions] = useState([])
     
+    let navigate = useNavigate();
+
+    const Logout = () => {
+        sessionStorage.removeItem('token');   
+        setToken('')
+        navigate('/signin')
+    }
 
     useEffect(() => {
+
+        let token = sessionStorage.getItem('token') || ''
+        
+        if(token.length == 0){
+            navigate('/signin')
+        }
+
         const GetAccountInfo = async () => {
             let token = sessionStorage.getItem('token') || '';
             await Get('account', token, setAccount);
@@ -38,6 +53,9 @@ const Home = () => {
         <Page>
             <Header>
                 <Logo src={LogoTotal}/>
+                <MenuItem onClick={Logout}>
+                    <Text>Sair</Text>
+                </MenuItem>
                 <MenuItem>
                     <Text>Sobre</Text>
                 </MenuItem>
@@ -61,7 +79,7 @@ const Home = () => {
                     <TitleCard>Transações</TitleCard>
                     <TransactionsList>
                         {
-                            transactions.map((obj, i) => <Transaction></Transaction>)
+                            transactions.map((transaction, i) => <Transaction transaction={transaction}></Transaction>)
                         }
                     </TransactionsList>
                 </TransactionsContainer>
