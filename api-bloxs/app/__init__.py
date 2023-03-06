08219f367340dcd68c60cca27b13a95c05580c2b
Gabriel <gabriel@quickup.io>
2023-03-05 23:30:47 -0300

    remove variavel global desnecessária

--- api-bloxs/app/__init__.py
+++ api-bloxs/app/__init__.py
@@ -3,27 +3,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_migrate import Migrate
-import pymysql
 
 db = SQLAlchemy()
-migrate = None
 
 def create_app():
-    global migrate
 
     app = Flask(__name__)
     migrate = Migrate(app, db)
-    
+
     config_name = os.getenv('FLASK_CONFIG') or 'default'
 
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/desafio'
     app.config['SECRET_KEY'] = 'akd3n13nf9canjfj6khvn'
-    
+
     config[config_name].init_app(app)
-    
+
     db.init_app(app)
     migrate.init_app(app)
 
     return app
-
